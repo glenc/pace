1637d013b9ee03d7e285442b54883714e0a25516
Glen Cooper <glencooper@gmail.com>
2013-10-22 22:54:03 -0700

    Added some queries and views

---
+++ server/domain/family/queries/queries.js
@@ -0,0 +1,49 @@
+var extend  = require('extend');
+var db      = require('../../../db');
+
+function query(name, defaultParams) {
+  return {
+    name: name,
+    model: 'family',
+    execute: function(view, parameters, callback) {
+      var p = extend(defaultParams, parameters);
+      var select = view.select || '';
+      db.Family.find(p, select, function(err, results) {
+        if (err) return callback(err);
+        if (view.map) {
+          results = results.map(view.map);
+        }
+        if (view.post) {
+          results = view.post(results);
+        }
+        callback(null, results);
+      });
+    }
+  };
+};
+
+function get() {
+  return {
+    name: 'get',
+    model: 'family',
+    execute: function(view, parameters, callback) {
+      var select = view.select || '';
+      db.Family.findById(parameters.id, function(err, doc) {
+        if (err) return callback(err);
+        if (view.map) {
+          doc = view.map(doc);
+        }
+        callback(null, doc);
+      });
+    }
+  }
+}
+
+module.exports = [
+  new query('', {}),
+  new query('active',   { status: 'Active' }),
+  new query('waitlist', { status: 'Waitlist' }),
+  new query('new',      { status: 'New' }),
+  new query('alumni',   { status: 'Alumni' }),
+  new get()
+];
---
+++ server/domain/family/views/views.js
@@ -0,0 +1,35 @@
+var _ = require('underscore');
+
+function view(name, select, map, post) {
+  return {
+    name: name,
+    model: 'family',
+    select: select,
+    map: map,
+    post: post
+  };
+};
+
+function idmap(f) {
+  var obj = f.toObject();
+  var id = f._id;
+  delete obj._id;
+  obj.id = id;
+  return obj;
+};
+
+module.exports = [
+  view('', 'id name', idmap),
+  view('detail', 'id name contacts', idmap),
+  view('contacts', 'contacts',
+    function(f) {
+      return f.contacts.map(function(c) {
+        var obj = idmap(c);
+        obj.family_id = f.id;
+        return obj;
+      });
+    },
+    function(all) {
+      return _.flatten(all);
+    })
+];
