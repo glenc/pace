ca5fc74f0c6f7bf9ddf46a2e0a2b8b25b5808380
Glen Cooper <glencooper@gmail.com>
2013-11-10 23:10:04 -0800

    Additional work on contact edit dialog

--- admin/app/scripts/controllers/contact-dialog.js
+++ admin/app/scripts/controllers/contact-dialog.js
@@ -4,6 +4,25 @@ angular.module('adminApp')
   .controller('ContactDialogCtrl', function ($scope, $modalInstance) {
 
     $scope.contact = {};
+    $scope.newNumber = { type: 'Mobile', number: '' };
+
+    $scope.addNumber = function() {
+      if (!$scope.contact.phoneNumbers) {
+        $scope.contact.phoneNumbers = [];
+      }
+      $scope.contact.phoneNumbers.push({
+        type: $scope.newNumber.type,
+        number: $scope.newNumber.number
+      });
+      $scope.newNumber.type = 'Mobile';
+      $scope.newNumber.number = '';
+    };
+
+    $scope.removeNumber = function(number) {
+      var idx = $scope.contact.phoneNumbers.indexOf(number);
+      if (idx > -1)
+        $scope.contact.phoneNumbers.splice(idx, 1);
+    };
 
     $scope.cancel = function() {
       $modalInstance.dismiss('cancel');
